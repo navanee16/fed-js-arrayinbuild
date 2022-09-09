7c95a2da7d8f9e25dca808fb59853ac7ab93e952
Navanee <navaneekrish164@gmail.com>
2022-09-09 13:50:42 +0530

    js

---
+++ ARRAYINBUILDMETHODS/arrayisarray/main.js
@@ -0,0 +1,20 @@
+let name =["navanee","navaneethan"];
+console.log(name);
+let num =598;
+console.log(num);
+console.log("method:array.arrayisarray()");
+console.log("name:",Array.isArray(name));
+console.log("num:", Array.isArray(num));
+
+
+let name1 = ["new","now"];
+console.log(name1);
+console.log(Array.isArray(name1));
+var x ="a";
+// var y = "b";
+var y = 4;
+if(x == y){
+    console.log("true");
+}else if ( x!= y){
+    console.log("false");
+}
